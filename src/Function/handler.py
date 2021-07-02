b22e830fb2022761d4b683c5e9bca69107e8eff0
Taras Glek <taras@glek.net>
2021-07-02 09:32:13 +0300

    works pretty well

--- src/Function/handler.py
+++ src/Function/handler.py
@@ -1,9 +1,15 @@
 import boto3
-import pymysql
-import os
 import logging
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+patch_all()
+
+import os
 import sys
 import json
+
 
 rds_host = os.environ['DB_ADDRESS']
 name = 'root'
@@ -11,21 +17,17 @@ password = '~<nqs6EUz;-ozaRP'
 db_name = 'db'
 port = os.environ['DB_PORT']
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+db_arn = os.environ['DB_ARN']
+secret_arn = os.environ['DB_ROOT_USER_SECRET_ARN']
 
-print("db info", json.dumps([rds_host,port, name, password, db_name ]))
-try:
-  conn = pymysql.connect(rds_host, port=port ,user=name, passwd=password, db=db_name, connect_timeout=10000)
-  print(conn)
-except:
-  logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
-  sys.exit()
-
-logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+print(db_arn, secret_arn)
+rdsData = boto3.client('rds-data')
 def handler(message, context):
-  with conn.cursor() as cur:
     sql = "SELECT * from sample_table"
-    cur.execute(sql)
-    for row in cur:
-      print(repr(row))
+    response1 = rdsData.execute_statement(
+            resourceArn = db_arn,
+            secretArn = secret_arn,
+            database = 'mydb',
+            sql = sql)
+    print(response1)
+    return {'statusCode':200, 'body':json.dumps(response1['records'])}
