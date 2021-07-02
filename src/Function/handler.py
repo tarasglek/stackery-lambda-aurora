56a2b2f25040803c0d6ea3b624643f3019c200aa
Taras Glek <taras@glek.net>
2021-07-02 10:19:36 +0300

    can trace sql statements

--- src/Function/handler.py
+++ src/Function/handler.py
@@ -5,29 +5,31 @@ from aws_xray_sdk.core import patch_all
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 patch_all()
-
+xray_recorder.configure(stream_sql=True)
 import os
 import sys
 import json
 
-
-rds_host = os.environ['DB_ADDRESS']
-name = 'root'
-password = '~<nqs6EUz;-ozaRP'
-db_name = 'db'
-port = os.environ['DB_PORT']
-
+db_name = 'mydb'
 db_arn = os.environ['DB_ARN']
 secret_arn = os.environ['DB_ROOT_USER_SECRET_ARN']
 
 print(db_arn, secret_arn)
 rdsData = boto3.client('rds-data')
+
+@xray_recorder.capture("## sql_bla")
+def sql(query):
+    # document = xray_recorder.current_segment()
+    xray_recorder.put_metadata("sql", query)
+    # xray_recorder.put_metadata("my key", "my value");
+    return rdsData.execute_statement(
+        resourceArn = db_arn,
+        secretArn = secret_arn,
+        database = db_name,
+        sql = query)
+
 def handler(message, context):
-    sql = "SELECT * from sample_table"
-    response1 = rdsData.execute_statement(
-            resourceArn = db_arn,
-            secretArn = secret_arn,
-            database = 'mydb',
-            sql = sql)
-    print(response1)
-    return {'statusCode':200, 'body':json.dumps(response1['records'])}
+    response1 = sql("SELECT COUNT(*) from sample_table")
+    response2 = sql("SELECT * from sample_table")
+    ret = [response1['records'], response2['records']]
+    return {'statusCode':200, 'body':json.dumps(ret)}
